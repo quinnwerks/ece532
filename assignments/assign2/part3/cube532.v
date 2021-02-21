1416d54b74cbf42505361bb531991fd02027ad69
Quinn Smith <quinn.smith98@gmail.com>
2021-02-21 13:56:33 -0500

    finish both implementations plus testbench

--- assignments/assign2/part3/cube532.v
+++ assignments/assign2/part3/cube532.v
@@ -5,6 +5,47 @@ module cube532
   output [23:0] out
 );
 
-// Your Code Here
+wire [31:0] rom_out;
+    
+rom rom_inst (
+    .clk(clk),
+    .raddr(in),
+    .data(rom_out)
+);
+
+assign out = rom_out[23:0];
+endmodule
+
+module cube532_golden (
+    input clk,
+    input [7:0] in,
+    output [23:0] out
+);
+    reg [23:0] reg_out;
+    reg [23:0] reg_out_2;
+    always @(posedge clk) begin
+        reg_out <= in*in*in;
+        reg_out_2 <= reg_out;
+    end
+    
+    assign out = reg_out_2;
+endmodule
+
+(* rom_style = "block" *)
+module rom (
+    input clk,
+    input [7:0]     raddr,
+    input [31:0]    data
+);
+reg [31:0] rom [255:0];
+reg [31:0] reg_data;
+reg [31:0] reg_data_2;
+
+initial $readmemh("cube.mem", rom);
+always @(posedge clk) begin
+    reg_data <= rom[raddr];
+    reg_data_2 <= reg_data;
+end
+assign data = reg_data_2;
 
 endmodule
---
+++ assignments/assign2/part3/cube532_tb.sv
@@ -0,0 +1,37 @@
+`timescale 1 ns / 1ps
+module cube532_tb();
+logic clk;
+logic [7:0] in;
+logic [23:0] out;
+logic [23:0] out_golden;
+
+int correct_out;
+
+initial clk = 1'b1;
+always #5 clk = ~clk;
+
+initial begin
+    for (int i = 0; i < 256; i++) begin
+        correct_out = i*i*i;
+        in = i;
+        // Takes one cycle to register input, another cycle for output to appear
+        @(posedge clk);
+        @(posedge clk);
+        assert(correct_out == out_golden && correct_out == out) else $stop("Values incorrect");
+    end
+end
+
+
+cube532 dut (
+    .clk(clk),
+    .in(in),
+    .out(out)
+);
+
+cube532_golden dut_golden (
+    .clk(clk),
+    .in(in),
+    .out(out_golden)
+);
+
+endmodule
