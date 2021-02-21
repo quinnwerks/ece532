7e061b50cee240865625ff93f8c5d150b9a0c23e
Quinn Smith <quinn.smith98@gmail.com>
2021-02-21 11:34:44 -0500

    infer a dsp

--- assignments/assign2/part2/preadd532.v
+++ assignments/assign2/part2/preadd532.v
@@ -1,3 +1,4 @@
+(* use_dsp48 = "yes" *)
 module preadd532
 #(
   parameter C_WIDTH = 16
@@ -11,7 +12,39 @@ module preadd532
   output signed [2*C_WIDTH: 0] out,
   output out_vld
 );
-
+reg [C_WIDTH-1:0] reg_a, reg_b, reg_c;
+reg [2*C_WIDTH:0] reg_out;
+reg reg_out_vld;
+reg reg_in_vld;
 // Your Code Here
+// Input
+always @(posedge clk) begin
+    // Input
+    if (in_vld) begin
+        reg_a <= in_a;
+        reg_b <= in_b;
+        reg_c <= in_c;
+        reg_in_vld <= 1'b1;
+    end
+    else begin
+        reg_a <= reg_a;
+        reg_b <= reg_b;
+        reg_c <= reg_c;
+        reg_in_vld <= 1'b0;
+    end
+    
+    // Output    
+    if (reg_in_vld) begin
+        reg_out_vld <= 1'b1;
+        reg_out <= (reg_a + reg_b) * reg_c;
+    end
+    else begin
+        reg_out_vld <= 1'b0;
+        reg_out <= reg_out;
+    end
+end
+
+assign out = reg_out;
+assign out_vld = reg_out_vld;
 
 endmodule
