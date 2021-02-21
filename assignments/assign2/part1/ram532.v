124dc05de04068c096d07db6ad9e0589921ebd49
Quinn Smith <quinn.smith98@gmail.com>
2021-02-21 11:09:48 -0500

    infer a ram

--- assignments/assign2/part1/ram532.v
+++ assignments/assign2/part1/ram532.v
@@ -1,3 +1,4 @@
+(* ram_style = "block" *)
 module ram532
 #(
   parameter C_ADDR_WIDTH = 8,
@@ -14,6 +15,25 @@ module ram532
   input  [C_ADDR_WIDTH-1: 0] raddr_1
 );
 
+reg [C_DATA_WIDTH-1:0] mem [(2**C_ADDR_WIDTH)-1:0];
+reg [C_DATA_WIDTH-1:0] data_0;
+reg [C_DATA_WIDTH-1:0] data_1;
+
 // Your Code Here
+// Read Logic
+always @(posedge clk) begin
+    data_0 <= mem[raddr_0];
+    data_1 <= mem[raddr_1];
+end
+
+// Write logic
+always @(posedge clk) begin
+    if (wen) begin
+        mem[waddr] <= wdata;
+    end
+end
+
+assign rdata_0 = data_0;
+assign rdata_1 = data_1;
 
 endmodule
